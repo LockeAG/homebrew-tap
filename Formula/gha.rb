14a560e87c6e71436e406d047ee69ad9e47c31c5
Adrian Galvez <8461849+LockeAG@users.noreply.github.com>
2026-03-18 15:02:46 -0600

    feat: gha v0.1.0-alpha with prebuilt binaries

--- Formula/gha.rb
+++ Formula/gha.rb
@@ -7,20 +7,20 @@ class Gha < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/LockeAG/gha/releases/download/v#{version}/gha-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "ee4ed75eca9b38c726912ea8bf06990ac5e9e655c7deb4b6cf3bc3b741d8ca9e"
     else
       url "https://github.com/LockeAG/gha/releases/download/v#{version}/gha-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "240eddc6542641b2e071c4086c804fe9d7b1729f2da69cb755037c5d8df8b51d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/LockeAG/gha/releases/download/v#{version}/gha-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "9fcefcfe46a117ae4fc622eb777e16300ff075fc81fc82fc933d811a548d23d2"
     else
       url "https://github.com/LockeAG/gha/releases/download/v#{version}/gha-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "7f77fe7321abe2ddc024b6dd92be1180e01e97e41a88391c6ffeae3ca2e7b151"
     end
   end
 
